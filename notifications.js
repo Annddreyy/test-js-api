7c8c53601e2e5a16b00c0e4e321da2b0f5cd47cf
Annddreyy <109622384+Annddreyy@users.noreply.github.com>
2025-02-15 20:50:07 +0300

    Add files via upload

--- notifications.js
+++ notifications.js
@@ -3,6 +3,8 @@
 let button = document.getElementById('button');
 button.onclick = () => requestPermission();
 
+window.onload = () => { showPermission(); };
+
 function requestPermission() {
 	Notification.requestPermission()
 		.then((permission) => {
@@ -13,3 +15,21 @@ function requestPermission() {
 			console.log(error);
 		});
 }
+
+function sendNotification() {
+	let title = 'Test';
+	let options = {
+	  body: 'Test body',
+	  // Other options can go here
+	};
+	console.log('Creating new notification');
+	let notification = new Notification(title, options);
+}
+
+function showPermission() {
+	let permission = Notification.permission;
+	console.log('Notification permission is ' + permission);
+	let p = document.getElementById('permission');
+	p.textContent = 'Notification permission is ' + permission;
+}
+
